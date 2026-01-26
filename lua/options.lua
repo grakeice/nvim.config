f5c88844851959917cc4fa074276b342e2c385ec
kurage <89824152+grakeice@users.noreply.github.com>
2026-01-26 17:48:05 +0900

    yankした行をハイライト

--- lua/options.lua
+++ lua/options.lua
@@ -2,6 +2,13 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 o.relativenumber = true
 o.breakindent = true
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
